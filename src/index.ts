7ecc2756c8d83e7819dff3b2ba0b3011c3a5eb66
fradotech <fradotech.id@gmail.com>
2024-04-01 16:58:32 +0700

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { CONFIG } from "./config";
 import { useDatabase } from "../database/database.connect";
 import userRouter from "./user.module";
@@ -8,17 +9,17 @@ import authMiddleware from "../infrastructure/middlewares/auth.middleware";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
 useDatabase();
 
 app.get("/", (_, res) => res.send("Spotlite book API!"));
 
-app.use(authMiddleware)
+app.use(authMiddleware);
 
 app.use(CONFIG.prefix, userRouter);
 app.use(CONFIG.prefix, bookRouter);
 app.use(CONFIG.prefix, orderRouter);
 
-app.listen(CONFIG.port, () =>
-  console.info("App is running on port ", CONFIG.port)
-);
+app.listen(CONFIG.port, () => console.info("App running port ", CONFIG.port));
